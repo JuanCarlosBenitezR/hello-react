ef476ec3cf494ab401459f5919d06703ba0af71b
JuanCarlosBenitezR <juan_carlos_2609@hotmail.com>
2025-04-19 20:51:00 -0600

    Pagination

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './Components/Navbar';
 import Characters from './Components/Characters';
+import Pagination from './Components/Pagination';
 function App() {
 	const [characters, setcharacters] = useState([]);
+	const [info, setinfo] = useState({});
 	const firstUrl = 'https://rickandmortyapi.com/api/character';
 	const fetchCharacters = (url) => {
 		fetch(url)
 			.then((response) => response.json())
-			.then((data) => setcharacters(data.results))
+			.then((data) => {
+				setcharacters(data.results);
+				setinfo(data.info);
+			})
 			.catch((error) => console.log(error));
 	};
 	useEffect(() => {
@@ -18,7 +23,9 @@ function App() {
 		<>
 			<Navbar brand={'Rick and morty app'}></Navbar>;
 			<div className="container mt-5">
+				<Pagination prev={info.prev} next={info.next} />
 				<Characters characters={characters}></Characters>
+				<Pagination></Pagination>
 			</div>
 			;
 		</>
